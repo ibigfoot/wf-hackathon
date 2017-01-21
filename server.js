99338a12f5f87b0bb17e4cde903338edbb7db9ea
tsellers <tsellers@salesforce.com>
2017-01-21 16:29:27 +0000

    fix

--- server.js
+++ server.js
@@ -4,14 +4,29 @@ var keypress = require('keypress')
 
 keypress(process.stdin)
 
-
 var state = {
-    journey : {}, // from TFL
+    journey : [], // from TFL
     currentStation : {},  // updated as we move through the journey
-    originStation : {},
-    destinationStation : {}
+    originStation : {
+        StationID:"9400ZZLUIMP",
+        StationName:"Imperial College",
+        Major:999,
+        InstancePrefix:"9999",
+        Latitude:51.498519,
+        Longitude:-0.177673,
+        WayfindrEnabled:true},
+    destinationStation : {
+        StationID:"9400ZZLUECT",
+        StationName:"Earls Court",
+        Major:63,
+        InstancePrefix:"003F",
+        Latitude:51.491876,
+        Longitude:-0.193526,
+        WayfindrEnabled:true}
 }
-
+// start journey
+// start beacon listener
+// 
 process.stdin.on('keypress', function (ch, key) {
     console.log('got keypress ['+JSON.stringify(key)+']')
     if (key && key.ctrl && key.name == 'c') {
