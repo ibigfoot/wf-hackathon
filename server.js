e4b5b0ba0c71f674d1600a2cdf9ffc3b4f3299b4
tsellers <tsellers@salesforce.com>
2017-01-21 15:01:08 +0000

    keypress

--- server.js
+++ server.js
@@ -1,9 +1,48 @@
 'use strict'
 
+var keypress = require('keypress')
+
+keypress(process.stdin)
+
+process.stdin.on('keypress', function (ch, key) {
+    console.log('got keypress ['+JSON.stringify(key)+']')
+    if (key && key.ctrl && key.name == 'c') {
+        process.stdin.pause();
+    }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+/*
+var baseAPI = 'http://wayfindr.azurewebsites.net/wayfindr'
 
 
-var stdin = process.openStdin();
-require('tty').setRawMode(true)
-stdin.on('keypress', function (chunk, key) {
-    process.stdout.write('Get chunk')
-})
+
+var apis = {
+    paths : '/Paths',
+    beacons : '/Beacons',
+    keys : '/Keys',
+    pois : '/POIs',
+    stations : '/Stations',
+    journeys : '/Journeys'
+}
+
+
+
+
+for (var key in apis) {
+    if ( apis.hasOwnProperty(key) ) {
+        console.log(key + ' => ' + apis[key]);
+        request(baseAPI + apis[key], function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log('----------------------------')
+                console.log(JSON.stringify(body))
+                console.log('----------------------------')
+            } else {
+                console.log(JSON.stringify(error))
+            }
+        })
+    }
+}
+*/
